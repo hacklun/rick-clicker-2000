961f03db5b7ee7fb4441afb87e4e0a4a78387e5d
hacklun <hacklun@mail.ru>
2020-09-09 11:35:10 +0300

    add images and start function addClicker

--- src/main.js
+++ src/main.js
@@ -5,3 +5,14 @@ function sync(dom, pos) {
     dom.style.left = `${pos.x}px`;
     dom.style.top = `${pos.y}px`;
 }
+
+function addClicker() {
+    const pos = {
+        x: Math.random() * 500,
+        y: Math.random() * 300
+    };
+
+    const img = new Image();
+    img.src = "res/images/rick.png";
+    
+}
